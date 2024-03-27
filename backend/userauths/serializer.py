4574bc3f1503c3d01974e96554efb0916badee04
Angelo <angelo.pucci@outlook.de>
2024-03-27 21:35:25 +0100

    Extend TokenObtainPairSerializer to Include Additional User Info
    - Created MyTokenObtainPairSerializer subclassing TokenObtainPairSerializer.
    
    - Customized get_token method to add 'username', 'email', 'full_name', and 'vendor_id' to the JWT token.
    
    - Included a try-except block to handle cases where 'vendor_id' might not be present, defaulting it to 0.

--- backend/userauths/serializer.py
+++ backend/userauths/serializer.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
 from .models import Profile, User
 
 
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super(MyTokenObtainPairSerializer, cls).get_token(user)
+        token['username'] = user.username
+        token['email'] = user.email
+        token['full_name'] = user.profile.full_name
+        try:
+            token['vendor_id'] = user.profile.vendor_id
+        except:
+            token['vendor_id'] = 0
+            
+        return token
 
 
 class UserSerializer(serializers.ModelSerializer):
