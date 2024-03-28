97a87b7b14332a1d344850fee29a5ee7d4b10ff9
Angelo <angelo.pucci@outlook.de>
2024-03-28 12:16:24 +0100

    Add Swagger API documentation with drf-yasg
    - Integrated drf-yasg for Swagger UI documentation.
    
    - Configured schema view with API details and set to public access.
    
    - Added Swagger UI endpoint to the project's URL patterns.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,9 +20,30 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="E-Commerce Backend API",
+        default_version='v1',
+        description="This is the docuemntation for the backend API",
+        terms_of_service="mywebsite.com/policys",
+        contact=openapi.Contact(email="angelo.pucci@outlook.de"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
+
+    # Documentation
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
