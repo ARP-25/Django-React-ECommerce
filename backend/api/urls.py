88588c621a4b7087490908ae426f25d4457d14de
Angelo <angelo.pucci@outlook.de>
2024-03-29 10:11:25 +0100

    Implement authentication flow in React with Django backend integration
    - Added auth.js in frontend/utils for login, register, logout, and token management functions.
    
    - Configured axios instance for API communication.
    
    - Updated Django backend to include JWT token refresh endpoint.
    
    - Integrated Zustand for state management in auth.js with useAuthStore.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from userauths import views as userauths_views
 from store import views as store_views
+
 
 urlpatterns = [
     path('user/token/', userauths_views.MyTokenObtainPairView.as_view()),
     path('user/register/', userauths_views.RegisterView.as_view()),
+    path('user/token/refresh/', TokenRefreshView.as_view()),
 ]
---
+++ frontend/src/utils/auth.js
@@ -0,0 +1,106 @@
+import { useAuthStore } from '../store/auth'
+import axios from 'axios'
+import jwt_decode from 'jwt-decode'
+import Cookie from 'js-cookie'
+
+
+
+export const login = async (email, password) => {
+    try {
+        const {data, status } = await axios.post("user/token", { email, password })
+
+        if (status===200) {
+            setAuthUser(data.access, data.refresh)
+            // Alert - Sign in Succesfully
+        }
+
+        return {data, error:null}
+    }
+    catch (error) {
+        return {data:null, error: error.response.data?.detail || error.message }
+    }
+}
+
+
+
+export const register = async (full_name, email, phone, password, password2) => {
+    try {
+        const { data } = await axios.post("user/register", { 
+            full_name,
+            email,
+            phone,
+            password,
+            password2 
+        })
+        await login(email, password)
+        // Alert - Signed up successfully
+        return { data, error: null }
+    }
+    catch (error){
+        return {data:null, error: error.response.data?.detail || error.message }
+    }
+}
+
+
+
+export const logout = () => {
+    Cookie.remove('access_token')
+    Cookie.remove('refresh_token')
+    useAuthStore.setState({ allUserData: null })
+    // Alert - You have been logged out
+}
+
+
+
+export const setUser = async () => {
+    const access_token = Cookie.get('access_token')
+    const refresh_token = Cookie.get('refresh_token')
+
+    if(!access_token && !refresh_token) {
+        return;
+    }
+
+    if(isAccessTokenExpired(access_token)) {
+        const response = await getRefreshToken(refresh_token)
+        setAuthUser(response.data.access, response.data.refresh)
+    } else {
+        setAuthUser(access_token, refresh_token)
+    }
+}
+
+
+
+export const setAuthUser = (access_token, refresh_token) => {
+    Cookie.set('access_token', access_token, {
+        expires: 1,
+        secure: true
+    })
+    Cookie.set('refresh_token', refresh_token, {
+        expires: 7,
+        secure: true
+    })  
+    const user = jwt_decode(access_token) ?? null
+    
+    if (user) {
+        userAuthStore.getState().setUser(user)
+    }
+    useAuthStore.getState().setLoading(false)
+}
+
+export const getRefreshToken = async () => {
+    const refresh_token = Cookie.get('refresh_token')
+    const response = await axios.post('user/token/refresh/', {
+        refresh: refresh_token
+    })
+    return response.data
+}
+
+export const isAccessTokenExpired = (access_token) => {
+    try { 
+        const decodedToken = jwt_decode(access_token)
+        return decodedToken.exp < Date.now() / 1000
+    } catch (error) {
+        console.log(error)
+        return false
+    }
+}
