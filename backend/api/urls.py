ec1be72c51b4d3470022514d3675480e1a237b80
Angelo <angelo.pucci@outlook.de>
2024-04-03 12:17:03 +0200

    Add Password Reset Functionality with Email OTP
    - Implement PasswordResetEmailVerificationView in userauths app
    
    - Extend from generics.RetrieveAPIView for handling password reset
    
    - Add OTP generation and email sending logic with reset link
    
    - Create API endpoint 'user/password-reset/<str:email>/'
    
    - Ensure security and user convenience in password recovery

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('user/token/', userauths_views.MyTokenObtainPairView.as_view()),
     path('user/register/', userauths_views.RegisterView.as_view()),
     path('user/token/refresh/', TokenRefreshView.as_view()),
+    path('user/password-reset/<str:email>/', userauths_views.PasswordResetEmailVerificationView.as_view()),
 ]
--- backend/userauths/views.py
+++ backend/userauths/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated, AllowAny
-
+from rest_framework.response import Response
 from userauths.models import User, Profile
-from userauths.serializer import MyTokenObtainPairSerializer, RegisterSerializer
+from userauths.serializer import MyTokenObtainPairSerializer, RegisterSerializer, UserSerializer
 
+import random
+import shortuuid
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
@@ -16,3 +18,32 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = [AllowAny,]
     serializer_class = RegisterSerializer
+
+
+def generate_otp(length=7):
+    uuid_key = shortuuid.uuid()
+    unique_key = uuid_key[:6]
+    return unique_key
+
+
+class PasswordResetEmailVerificationView(generics.RetrieveAPIView):
+    permission_classes = [AllowAny,]
+    serializer_class = UserSerializer
+    
+    def get_object(self):
+        email = self.kwargs.get('email')
+        user = User.objects.get(email=email)
+
+        if user:
+            otp = generate_otp()
+            user.otp = otp
+            user.save()
+
+            uidb64 = user.pk
+            otp = user.otp
+
+            link = f"http://localhost:5173/create-new-password?otp={otp}&uidb64={uidb64}"
+            print("link ======", link)
+
+        return user
+    
